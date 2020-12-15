67f9569f6032749188f4bd8340d4cad270182844
Payton <greatpayton@gmail.com>
2020-12-14 22:06:26 -0700

    debugging

--- assets/giphy.js
+++ assets/giphy.js
@@ -11,7 +11,11 @@ let topics = [
   'Corvette',
 ];
 
+const APIKey = '&api_key=3meK41RxJiNeS6c81jqYQ5Fzqtiq0p0j&limit=10';
+const urlBase = 'https://api.giphy.com/v1/gifs/search?q=';
+let giphyName = '';
 const buttonArea = document.querySelector('.button-area');
+const imageContainer = document.querySelector('.images-container');
 
 function generateButtons() {
   for (topic of topics) {
@@ -23,85 +27,23 @@ function generateButtons() {
 }
 
 document.addEventListener('click', (e) => {
-  e.target.classList.contains('btn-options') ? callAPI(e.target) : null;
+  if (e.target.classList.contains('btn-options')) {
+    giphyName = e.target.textContent;
+    getGiphy();
+  } else {
+    return null;
+  }
 });
 
-async function callAPI(item) {
-  const APIKey = 'api_key=3meK41RxJiNeS6c81jqYQ5Fzqtiq0p0j&limit=10';
-  const response = await fetch(
-    `https://api.giphy.com/v1/gifs/search?q=${item.textContent}&${APIKey}`
-  );
-  const data = await response.json();
+async function getGiphy() {
+  const url = urlBase + giphyName + APIKey;
+  const giphyAPI = await fetch(url);
+  const giphy = await giphyAPI.json();
+  // console.log(giphy);
 
-  return data;
+  for (item in giphy) {
+    console.log(giphy);
+  }
 }
+
 generateButtons();
-
-// Generating button
-// $(function() {
-//   for (let i = 0; i < topics.length; i++)
-//     $('#button-area').append(
-//       $('<button>')
-//         .text(topics[i])
-//         .addClass('btn btn-light searchButton')
-//         .attr('data-type', topics[i])
-//     );
-// });
-
-// // On click event to add GIPHY to search area
-// $(document).on('click', '.searchButton', function() {
-//   let type = $(this).data('type');
-
-//   let APIKey = '&api_key=3meK41RxJiNeS6c81jqYQ5Fzqtiq0p0j&limit=10';
-//   let queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + type + APIKey;
-
-//   $.ajax({
-//     url: queryURL,
-//     method: 'GET'
-//   }).then(function(response) {
-//     console.log(response);
-//     for (let i = 0; i < response.data.length; i++) {
-//       let gifDiv = $('<div>');
-//       let still = response.data[i].images.fixed_height_still.url;
-//       let animate = response.data[i].images.fixed_height.url;
-//       let rating = response.data[i].rating;
-//       let p = $('<p>').text('Rating: ' + rating);
-//       let image = $('<img>');
-//       image.addClass('gif-image');
-//       image.attr('src', still);
-//       image.attr('data-still', still);
-//       image.attr('data-animate', animate);
-//       image.attr('data-state', 'still');
-//       gifDiv.append(p);
-//       gifDiv.append(image);
-//       $('#search-area').prepend(gifDiv);
-//     }
-//   });
-// });
-
-// // Search button functionality
-
-// $('#search-button').on('click', function() {
-//   let addSearch = $('input').val();
-//   topics.push(addSearch);
-//   $('#button-area').append(
-//     $('<button>')
-//       .text(addSearch)
-//       .addClass('btn btn-light searchButton')
-//       .attr('data-type', addSearch)
-//   );
-//   return false;
-// });
-
-// // Animage and still functionality
-
-// $(document).on('click', '.gif-image', function() {
-//   let state = $(this).attr('data-state');
-//   if (state === 'still') {
-//     $(this).attr('src', $(this).data('animate'));
-//     $(this).attr('data-state', 'animate');
-//   } else {
-//     $(this).attr('src', $(this).data('still'));
-//     $(this).attr('data-state', 'still');
-//   }
-// });
