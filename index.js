c9455bae7c4801915bd113d2bd7a681c9d4d209f
payton-burr <greatpayton@gmail.com>
2021-05-26 20:56:51 -0600

    added trending gifs on load

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const key = '&api_key=3meK41RxJiNeS6c81jqYQ5Fzqtiq0p0j&limit=10';
-const url = 'https://api.giphy.com/v1/gifs/search?q=';
+const url = 'https://api.giphy.com/v1/gifs/';
 const button = document.querySelector('button');
 const input = document.querySelector('input');
 const results = document.querySelector('.results');
 
 const fetchData = async (search) => {
-  const urlBase = url + search + key;
-  const response = await fetch(urlBase);
-  return await response.json();
+  let urlBase;
+  !search
+    ? (urlBase = `${url}trending?${key}`)
+    : (urlBase = `${url}search?q=${search}${key}`);
+
+  try {
+    const response = await fetch(urlBase);
+    return await response.json();
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 const handleData = async (e) => {
@@ -31,5 +39,7 @@ const handleData = async (e) => {
     });
   });
 };
+
+window.addEventListener('load', handleData);
 
 button.addEventListener('click', handleData);
